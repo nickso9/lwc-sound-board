b8c817d7a68b0e9d87996c658ba2c77c93c90271
nicksandoval <sand8val@gmail.com>
2023-04-27 17:31:43 -0500

    audio playing

--- force-app/main/default/lwc/soundboard/soundboard.js
+++ force-app/main/default/lwc/soundboard/soundboard.js
@@ -1,4 +1,4 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import Id from '@salesforce/user/Id'
 import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
 import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
@@ -9,7 +9,9 @@ import ACCOUNT from "@salesforce/schema/User.Account.Soundboard__c";
 import SOUNDBOARD_ID from "@salesforce/schema/User.Account.Soundboard__r.Id";
 
 import IMG_URL from "@salesforce/schema/Sound__c.Audio_IMG__c";
-import AUDIO_URL from "@salesforce/schema/Sound__c.Audio_URL__c";
+import AUDIO_URL from "@salesforce/schema/Sound__c.Audio_Src__c";
+import SOUND_NAME from "@salesforce/schema/Sound__c.Name";
+import SOUND_ID from "@salesforce/schema/Board_Audio__c.Id";
 
 const USER_FIELDS = [Name, Alias, AccountId, ACCOUNT, SOUNDBOARD_ID];
 
@@ -18,6 +20,8 @@ export default class Soundboard extends LightningElement {
 
     recordId = '005Do0000023NbZIAU'
     soundboardId;
+    @track
+    sounds = [];
 
     @wire(getRecord, { recordId: '$recordId', fields: USER_FIELDS })
     accountHandler({ data, error }) {
@@ -33,8 +37,8 @@ export default class Soundboard extends LightningElement {
     @wire(getRelatedListRecords, {
         parentRecordId: '$soundboardId',
         relatedListId: 'Board_Audios__r',
-        fields: ['Board_Audio__c.Id', 'Board_Audio__c.Name', 'Board_Audio__c.Sound__r.Audio_IMG__c', 'Board_Audio__c.Sound__r.Audio_URL__c'],
-        sortBy: ['Board_Audio__c.Name']
+        fields: ['Board_Audio__c.Id', 'Board_Audio__c.Sound__r.Name', 'Board_Audio__c.Sound__r.Audio_IMG__c', 'Board_Audio__c.Sound__r.Audio_Src__c'],
+        sortBy: ['Board_Audio__c.Sound__r.Name']
     })
     relatedSoundHandler({ data, error }) {
         if (data) {
@@ -48,16 +52,27 @@ export default class Soundboard extends LightningElement {
         const totalSounds = data.length;
         // console.log(data);
         const sounds = [...data];
-        // console.log(IMG_URL)
+        const tempSound = [];
         for (let i = 0; i < totalSounds; i++) {
             const { Sound__r: sound } = sounds[i].fields;
-            // console.log(sound)
             const audio = getFieldValue(sound.value, AUDIO_URL);
             const img = getFieldValue(sound.value, IMG_URL);
-            console.log(audio);
-            console.log(img);
+            const name = getFieldValue(sound.value, SOUND_NAME);
+            const id = getFieldValue(sounds[i], SOUND_ID)
+            tempSound.push({id, name, img, audio});
         }
+        // console.log(JSON.parse(JSON.stringify(tempSound)));
+        this.sounds = tempSound;
+        // console.log(JSON.parse(JSON.stringify(this.sounds)));
+        // console.log(this.sounds)
     }
 
+    audioHandler(event) {
+        console.log('click')
+        const id = "#" + event.target.id;
 
+        const audioContainer = this.template.querySelector(id);
+        const audio = audioContainer.firstElementChild
+        audio.play();
+    }
 }
