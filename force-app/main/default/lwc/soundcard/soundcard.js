68aa85233d56a47046d2a9e17d994741925dc401
nicksandoval <sand8val@gmail.com>
2023-04-29 23:16:05 -0500

    card

--- force-app/main/default/lwc/soundcard/soundcard.js
+++ force-app/main/default/lwc/soundcard/soundcard.js
@@ -4,9 +4,12 @@ export default class Soundcard extends LightningElement {
     @api sounds;
 
     audioHandler(event) {
-        const id = "#" + event.currentTarget.id;
-        const audioContainer = this.template.querySelector(id);
-        const audio = audioContainer.firstElementChild
-        audio.play();
+    
+        const audio = event.currentTarget.closest('audio');
+        console.log(event.currentTarget.firstChild.children[1])
+        // const id = "#" + event.currentTarget.id;
+        // const audioContainer = this.template.querySelector(id);
+        // const audio = audioContainer.firstElementChild
+        // audio.play();
     }
 }
