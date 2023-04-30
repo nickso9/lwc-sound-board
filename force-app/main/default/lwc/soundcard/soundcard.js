a0db133d68632c21191053a6cbd20005e75cc25b
nicksandoval <sand8val@gmail.com>
2023-04-30 15:31:40 -0500

    delete working

--- force-app/main/default/lwc/soundcard/soundcard.js
+++ force-app/main/default/lwc/soundcard/soundcard.js
@@ -1,10 +1,12 @@
-import { LightningElement, api} from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { deleteRecord } from 'lightning/uiRecordApi';
+// import removedSound from "@salesforce/apex/SoundController.removedSound";
 
 export default class Soundcard extends LightningElement {
     @api sounds;
 
-    audioHandler(event) {
-    
+    async actionHandler(event) {
+
         const currentEle = event.currentTarget
         const action = currentEle.getAttribute("data-action")
 
@@ -16,11 +18,17 @@ export default class Soundcard extends LightningElement {
         if (action === "delete") {
             console.log("delete now")
             const parent = currentEle.parentElement;
-            console.log(parent.id);
-            console.log(parent)
+            const soundElemId = parent.id;
+            const soundId = soundElemId.slice(0, 18);
+            try {
+                const deletedRecord = await deleteRecord(soundId)
+                console.log(deletedRecord)
+            } catch (error) {
+                console.log(error);
+            }
         }
 
     }
-    
+
 
 }
