61e1625d3a0f7a1133172ce3306199b2432d4095
nicksandoval <sand8val@gmail.com>
2023-05-03 18:18:45 -0500

    added image

--- force-app/main/default/lwc/createSound/createSound.js
+++ force-app/main/default/lwc/createSound/createSound.js
@@ -2,23 +2,43 @@ import { LightningElement, api } from 'lwc';
 
 export default class CreateSound extends LightningElement {
 
-    @api documentId;
+    audioDocumentId;
+    imageDocumentId;
     audioLibrary = '058Do000000kF82IAE';
+    imageLibrary = '058Do000000kFGCIA2';
 
-    get audioLibrary() {
-        return this.audioLibrary
+    get imageLibrary() {
+        return this.imageLibrary;
     }
 
-    get acceptedFormats() {
+    get audioLibrary() {
+        return this.audioLibrary;
+    }
+
+    get acceptedAudioFormats() {
         return ['.mp3'];
     }
 
+    get acceptedImageFormats() {
+        return ['.png', '.jpg', '.gif'];
+    }
+
     handleUploadFinished(event) {
-        console.log('uploaded files');
         const files = event.detail.files;
-        console.log(files[0].documentId)
-        this.documentId = files[0].documentId
-        console.log('uploaded files123');
+        if (files[0].mimeType.includes('image')) {
+            console.log('image was uploaded')
+            this.imageDocumentId = files[0].documentId
+
+        } else if (files[0].mimeType.includes('audio')) {
+            console.log('audio was uploaded')
+            this.audioDocumentId = files[0].documentId;
+
+        }
+        // console.log(JSON.parse(JSON.stringify(event.detail.files)))
+        console.log('uploaded files');
+    
+        // console.log(files[0].documentId)
+        // console.log('uploaded files123');
     }
 
     // test(data) {
@@ -30,3 +50,10 @@ export default class CreateSound extends LightningElement {
 
     }
 }
+
+/**
+ * 
+ * DELETE FROM ContentDocument WHERE OwnerId = '005Do0000023MItIAM'
+ * 
+ * 
+ */
